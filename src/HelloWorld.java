2e78a63f2261fee7e9a17c69ffd369c28ea14826
const1995 <KMakarov@rstyle.kiev.ua>
2018-07-24 19:51:33 +0300

    second commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,12 @@
 package com.rstyle;
 
+
+
 public class HelloWorld
 {
     public static void main(String[] args)
     {
         System.out.println("Hello, world!");
+        System.out.println("Hello, Git!");
     }
 }
