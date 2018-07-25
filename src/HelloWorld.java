859eba5a8bce30d7a002f3a4431fd1f3bab967fc
const1995 <KMakarov@rstyle.kiev.ua>
2018-07-25 15:05:32 +0300

    third edition

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, world!");
+        System.out.println("Hello, future world!");
         System.out.println("Hello, Git!");
     }
 }
